dd6aede1e2ba43316892b5b55b61bfb9bfb8ce44
Enrique Sapien <esapien@hotmail.com>
2018-09-09 18:21:16 -0500

    Add for loop to add elements to list

--- vue-sample.js
+++ vue-sample.js
@@ -2,12 +2,16 @@ new Vue({
     el: '#app',
     data: {
         message: 'Hello World',
-        messageUpdated: false
+        messageUpdated: false,
+        elements: []
     },
     methods: {
         changeMessage: function() {
             this.message = 'Hi';
             this.messageUpdated = true;
+        },
+        addNewElement: function() {
+            this.elements.push(this.elements.length + 1);
         }
     }
 });
