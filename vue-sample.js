0aa3a8f344f0628e1ace5fadaa26858cfa7c120e
Enrique Sapien <esapien@hotmail.com>
2018-09-09 18:12:56 -0500

    Add conditional rendering

--- vue-sample.js
+++ vue-sample.js
@@ -1,11 +1,13 @@
 new Vue({
     el: '#app',
     data: {
-        message: 'Hello World'
+        message: 'Hello World',
+        messageUpdated: false
     },
     methods: {
         changeMessage: function() {
             this.message = 'Hi';
+            this.messageUpdated = true;
         }
     }
 });
